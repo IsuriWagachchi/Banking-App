5559e4d07601e41a7a96273cec06a7068d70226c
Isuri Wagachchi <isuriwagachchi19@gmail.com>
2026-01-19 14:24:20 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //BANKING PROGRAM SAMPLE
+
+        //DECLARE VARIABLES
+
+        //DISPLAY MENU
+
+        //GET AND PROCESS USERS CHOICES
+
+        //showbalace()
+
+        //deposit()
+
+        //withdraw()
+
+        //EXIT MESSAGE
+
+
+        System.out.println("Hello world!");
+    }
+}
